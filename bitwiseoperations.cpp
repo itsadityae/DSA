d8d9e5249a2331a7f6ae6859cbbc1dc5e956d1ef
itsadityae <adityaedq@gmail.com>
2025-02-22 23:45:24 +0530

    Initial commit

--- bitwiseoperations.cpp
+++ bitwiseoperations.cpp
@@ -6,8 +6,8 @@ int main()
     int a = 4, b = 8;
     
     cout << "The Bitwise AND operation of 4 and 8 is" << (a & b) << endl; // Output : 0
-    cout << "The Bitwise OR operation of 4 and 8 is" << (a | b) << endl;  // 12
-    cout << "The Bitwise XOR operation of 4 and 8 is" << (a ^ b) << endl; // 12
+    cout << "The Bitwise OR operation of 4 and 8 is" << (a | b) << endl;  // Output : 12
+    cout << "The Bitwise XOR operation of 4 and 8 is" << (a ^ b) << endl; // Output : 12
 
     return 0;
 }
