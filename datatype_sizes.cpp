b313a124b3b916cc2a1a18271649c3918af9d658
itsadityae <adityaedq@gmail.com>
2025-02-22 23:44:16 +0530

    Initial commit

--- datatype_sizes.cpp
+++ datatype_sizes.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
+    
     cout << "Size of char: " << sizeof(char) << " bytes" << endl;
     cout << "Size of int: " << sizeof(int) << " bytes" << endl;
     cout << "Size of float: " << sizeof(float) << " bytes" << endl;
